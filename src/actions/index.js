6a1b50e64dff986cb4959169957bc17ca9f9bb44
Nadeem Ansari <s524968@mail.nwmissouri.edu>
2017-05-18 16:36:17 -0500

    Added Axios to write AJAX requests to OpenWeatherMap API

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,15 @@
+import axios from 'axios';
 const API_KEY = '64e5121d7725dbead84248c8ea3645c0';
+const ROOT_URL = `http://samples.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+    const url = `${ROOT_URL}&q=${city},us`;
+    const request = axios.get(url);
+
+    return {
+        type: FETCH_WEATHER,
+        payload: request
+    };
+}
