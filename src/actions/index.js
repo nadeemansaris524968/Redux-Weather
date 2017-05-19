dbd121fef19d95f2c25d3a2407d7263d01585f06
Nadeem Ansari <s524968@mail.nwmissouri.edu>
2017-05-18 22:52:07 -0500

    Added WeatherReducer

--- src/actions/index.js
+++ src/actions/index.js
@@ -5,9 +5,14 @@ const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KE
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
 export function fetchWeather(city) {
+
+    // Asynchronous 
     const url = `${ROOT_URL}&q=${city},us`;
     const request = axios.get(url);
 
+    // when the action's payload is a Promise ( here request is a redux promise )
+    // middleware stops it, unwraps the promise into reducer usable data and then
+    // sends it out to all the reducers.
     return {
         type: FETCH_WEATHER,
         payload: request
