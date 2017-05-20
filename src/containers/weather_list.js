9efb320974be1efe9f17c4d8849c581c9f8c932b
Nadeem Ansari <s524968@mail.nwmissouri.edu>
2017-05-20 15:43:04 -0500

    Added table to display city weather info
    Each row is a city. Newly searched cities are added to the top. Temperature graphs are displayed using React-Sparklines library.

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,29 +1,47 @@
 import React, { Component } from 'react';
-import {connect} from 'react-redux';
+import { connect } from 'react-redux';
+import { Sparklines, SparklinesLine } from 'react-sparklines';
 
 class WeatherList extends Component {
+        // Renders a city row
+        renderWeather(cityData) {
+            const name = cityData.city.name;
+            const temps = cityData.list.map(weather => weather.main.temp);
+            console.log(temps);
 
-render() {
-    return (
-        <table className="table table-hover">
-            <thead>
-                <tr>
-                    <th>City</th>
-                    <th>Temperature</th>
-                    <th>Pressure</th>
-                    <th>Humidity</th>
+            return (
+                <tr key={name}>
+                    <td>{name}</td>
+                    <td>
+                        <Sparklines height={120} width={180} data={temps} >
+                            <SparklinesLine color="red" />
+                        </Sparklines>
+                    </td>
                 </tr>
-            </thead>
-            <tbody>
-            </tbody>
-        </table>
-        );
+            );
+        }
+        render() {
+            return (
+                <table className="table table-hover">
+                    <thead>
+                        <tr>
+                            <th>City</th>
+                            <th>Temperature</th>
+                            <th>Pressure</th>
+                            <th>Humidity</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {this.props.weather.map(this.renderWeather)}
+                    </tbody>
+                </table>
+            );
+        }
     }
-}
 
 // Extracting the state.weather from state and adding it 
 // current containers props. ES6 syntax.
-function mapStateToProps({weather}) {
+function mapStateToProps({ weather }) {
     return { weather };
 }
 
