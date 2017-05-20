9fddef845926ccc3c62849fc5d596fa989cbe8d1
Nadeem Ansari <s524968@mail.nwmissouri.edu>
2017-05-20 13:03:26 -0500

    WeatherReducer now handles the unwrapped data send by Redux Promise response

--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -1,3 +1,10 @@
-export default function(state = null, action) {
+import { FETCH_WEATHER } from '../actions/index';
+
+export default function(state = [], action) {
+    switch (action.type) {
+        case FETCH_WEATHER:
+        return [action.payload.data, ...state]; // adds the city to the existing list of cities and returns a new 
+        // state. Never modify a state. [city, city, city]
+    }
     return state;
 }
